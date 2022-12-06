0588f5c46e2f6a31c4ec0d00928467716a6fe6f0
Aleksandar Dimitrov <git@aleks.bg>
2022-12-06 20:23:58 +0100

    Use streams to improve performance

--- racket/2022-06.rkt
+++ racket/2022-06.rkt
@@ -1,15 +1,13 @@
 #lang racket
 
+(require racket/stream)
 (require rackunit)
 
 (define (sequences str unique-len)
-  (define (iter s lst)
-    (if ((string-length s) . <= . unique-len)
-        (cons s lst)
-        (iter (substring s 1) (cons (substring s 0 unique-len) lst))))
-  (reverse (iter str '())))
+  (for/stream ([i (range 0 (+ 1 (- (string-length str) unique-len)))])
+    (substring str i (+ i unique-len))))
 
-(check-equal? (sequences "abcdefg" 4)
+(check-equal? (stream->list (sequences "abcdefg" 4))
               '("abcd" "bcde" "cdef" "defg"))
 
 (define (check-signal str)
@@ -19,10 +17,18 @@
 (check-equal? (check-signal "abcd") #t)
 (check-equal? (check-signal "abca") #f)
 
+(define (stream-index-of p s)
+  (define (iter s n)
+    (if (eq? p (stream-first s))
+        n
+        (iter (stream-rest s) (+ 1 n))))
+  (iter s 0))
+
+(check-equal? (stream-index-of 10 (for/stream ([i (in-naturals)]) i)) 10)
+
 (define (run-task unique-len str )
   (+ unique-len
-     (index-of (map check-signal (sequences str unique-len))
-               #t)))
+     (stream-index-of #t (stream-map check-signal (sequences str unique-len)))))
 
 (check-equal? (run-task 4 "bvwbjplbgvbhsrlpgdmjqwftvncz") 5)
 (check-equal? (run-task 4 "nppdvjthqldpwncqszvftbrmjlhg") 6)
